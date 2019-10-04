4f8f7392b07f4d5f188faaa8bc68fe922eea808a
신용주 <lukas.sin@kakaocorp.com>
2019-10-04 19:06:47 +0900

    ADDING scroll Event handler in nav

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,19 +4,33 @@ import './Header.scss'
 const Header = () => {
   const title = 'PARRALAX'  
 
-  const pageList = document.querySelectorAll('.page');
-  const clickHandler = (e)  => {
-    console.log(pageList[e.currentTarget.dataset['id']])
+  
+  const clickHandler = (item)  => {
+   
+    console.log(item.dataset.pos, window.scrollY )
+    window.scrollY = item.dataset.pos + 'px'
   }
+  
+  const [nav , setNav]  = React.useState(null)
+
+  React.useEffect(()=>{
+    const pageList = document.querySelectorAll('.page');
+    const nav = Array.from(pageList).map((item,index)=>{
+      return (
+        <li key={index}><button onClick={(e)=>clickHandler(item)} data-id={index}>{'page'+index}</button></li>
+      )
+    })
+    setNav(nav)
+  },[])
   return (
   <header >
     <h1>{title}</h1>
     <nav>
       <ul>
-        <li><button onClick={clickHandler} data-id={0}>page1</button></li>
+        {/* <li><button onClick={clickHandler} data-id={0}>page1</button></li>
         <li><button onClick={clickHandler} data-id={1} >page2</button></li>
-        <li><button onClick={clickHandler} data-id={2} >page3</button></li>
-        <li><button onClick={clickHandler} data-id={3} >page4</button></li>
+        <li><button onClick={clickHandler} data-id={2} >page3</button></li> */}
+        {nav}
       </ul>
     </nav>
   </header>

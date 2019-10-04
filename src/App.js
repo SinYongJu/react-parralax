523dab7d2f5728c442850c89f03d38b8a718b334
신용주 <lukas.sin@kakaocorp.com>
2019-10-04 10:02:28 +0900

    init this folder

--- src/App.js
+++ src/App.js
@@ -2,64 +2,10 @@ import React from 'react';
 import ParralaxPage from './pages/ParralaxPage'
 import Header from './components/Header'
 
+
 const  App = () => {
-
-  const [isInitState, actionIsInitState] = React.useState(null)
-  const [LODED,LODING]= ['LOADED' , 'LODING'];
-
-  const handler = (e)=>{ 
-    if(e){
-      console.log(e)
-    }
-    if(isInitState === LODED ){
-      actionIsInitState(LODING)
-    }else{
-      actionIsInitState(LODED)
-    }
-   
-  }  
-  const scrollHandler = (e) => {
-    setTimeout(()=>{
-      console.log('scroll')
-      // if(e){
-      //   console.log(e)
-      // }
-      // if(isInitState === LODED ){
-      //   actionIsInitState(LODING)
-      // }else{
-      //   actionIsInitState(LODED)
-      // }
-     
-    },500)
-  }
-
-  document.addEventListener('DOMContentLoaded',handler);
-
-  const removeHandler = () => {
-    document.removeEventListener("scroll",scrollHandler)
-    window.removeEventListener("resize",scrollHandler)
-    document.removeEventListener('DOMContentLoaded',handler);
-  }
-  const handlerCallback = React.useCallback(removeHandler,[])
-  
-  React.useEffect(()=>{
-    document.addEventListener("scroll",scrollHandler)
-    window.addEventListener("resize",handler)
-    console.log('loading..')
-     return handlerCallback
-  },[isInitState])
-
-  // document.addEventListener("scroll",()=>{
-  //   console.log('scroll')
-  // })
-  // window.addEventListener("resize",()=>{
-  //   console.log('resize')
-  // })
   return (
-    <>
-      <Header></Header>
-      <ParralaxPage isInitState={isInitState}></ParralaxPage>
-    </>
+      <ParralaxPage />
   );
 }
 
--- src/pages/ParralaxPage.js
+++ src/pages/ParralaxPage.js
@@ -2,21 +2,13 @@ import React from 'react';
 import ParralaxTemplate from './template/ParralaxTemplate'
 import TitleDescImgPageComp from '../components/TitleDescImgPageComp'
 
-const ParralaxPage = ({isInitState}) => {
-  const [imgSrc01, imgSrc02, imgSrc03, imgSrc04, imgSrc05] = isInitState === 'LOADED' ? ['./images/bg_todo01.jpg','./images/bg_todo02.jpg','./images/bg_todo03.jpg','./images/bg_todo04.jpg','./images/bg_todo05.jpg'] : []
+
+
+const ParralaxPage = (props) => {
+
+  
   return (
-    isInitState === 'LOADED' ? 
-    <ParralaxTemplate
-      page1={<TitleDescImgPageComp imgSrc={imgSrc01}/>}  
-      page2={<TitleDescImgPageComp imgSrc={imgSrc02}/>}  
-      page3={<TitleDescImgPageComp imgSrc={imgSrc03}/>}  
-      page4={<TitleDescImgPageComp imgSrc={imgSrc04}/>}  
-      page5={<TitleDescImgPageComp imgSrc={imgSrc05}/>}  
-    /> :
-    <div style={{'fontSize':'180px'}}>
-      <h2>loading 중</h2>
-    </div>
-  );
+    <ParralaxTemplate />)
 };
 
 export default ParralaxPage;
